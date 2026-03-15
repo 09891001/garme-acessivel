f5d58ccedaa2eb0e982800b9653aa4dca6d4c716
09891001 <rochaventura99@gmail.com>
2026-03-15 16:17:27 -0300

    criando as primeiras partes do jogo

---
+++ js/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxav0baX6bucdYUlw1pRWOFcv9AwtqymY",
+  authDomain: "garme-acessivel.firebaseapp.com",
+  projectId: "garme-acessivel",
+  storageBucket: "garme-acessivel.firebasestorage.app",
+  messagingSenderId: "69205141908",
+  appId: "1:69205141908:web:d3ce0b770f699c1a8ac781"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
